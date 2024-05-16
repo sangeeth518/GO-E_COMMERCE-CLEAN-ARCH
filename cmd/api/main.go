fbe28aa415ffa2d50fd62ef535af8a82a15dc820
Sangeeth <sangeethsajeev518@gmail.com>
2024-05-16 19:33:53 +0530

    load config func added got data from env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/sangeeth518/go-Ecommerce/pkg/config"
+)
+
 func main() {
 
+	config, configerr := config.LoadConfig()
+	if configerr != nil {
+		log.Fatal("cannot load config", configerr)
+	}
+
+	fmt.Printf(config.DBHost)
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import "github.com/spf13/viper"
+
+type config struct {
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBName     string `mapstructure:"DB_NAME"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+}
+
+var envs = []string{
+	"DB_HOST", "DB_NAME", "DB_PORT", "DB_USER", "DB_PASSWORD",
+}
+
+func LoadConfig() (config, error) {
+	var config config
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
+	for _, val := range envs {
+		if err := viper.BindEnv(val); err != nil {
+			return config, err
+		}
+	}
+	if err := viper.Unmarshal(&config); err != nil {
+		return config, err
+	}
+	return config, nil
+
+}
