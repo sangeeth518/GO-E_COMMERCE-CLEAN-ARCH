80dcc607cae6acd53f87d2e06a57d3651f4ef676
Sangeeth <sangeethsajeev518@gmail.com>
2024-06-27 23:54:39 +0530

    models categroy added

---
+++ pkg/domain/inventory.go
@@ -0,0 +1,17 @@
+package domain
+
+type Category struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Inventories struct {
+	Id          int      `json:"id" gorm:"unique;not null" `
+	CategoryId  int      `json:"categroy_id"`
+	Category    Category `json:"-" foreignkey:"CategoryId"`
+	ProductName string   `json:"product_name"`
+	Image       string   `json:"image"`
+	Size        string   `json:"size" gorm:"size:5;default:'M';check:size IN ('S', 'M', 'L', 'XL')"`
+	Stock       int      `json:"stock"`
+	Price       float64  `json:"price"`
+}
